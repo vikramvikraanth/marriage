522989ddba4ec0587f40f51fb0e6664585063dac
vikram <vikramvikraanth4@gmail.com>
2018-05-09 14:10:50 -0700

    new page added

--- app/src/main/java/demo/check/com/MainActivity.java
+++ app/src/main/java/demo/check/com/MainActivity.java
@@ -1,18 +1,27 @@
 package demo.check.com;
 
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import nl.dionsegijn.konfetti.KonfettiView;
-import nl.dionsegijn.konfetti.models.Shape;
+import butterknife.OnClick;
+import demo.check.com.Fragments.MapScreen;
+import demo.check.com.Fragments.WelcomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.viewKonfetti)
-    KonfettiView viewKonfetti;
+
+    @BindView(R.id.welcome_btn)
+    Button welcomeBtn;
+    @BindView(R.id.time_line_btn)
+    Button timeLineBtn;
+    @BindView(R.id.venue_btn)
+    Button venueBtn;
+   static Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+
+    }
+
+    @OnClick({R.id.welcome_btn, R.id.time_line_btn, R.id.venue_btn})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.welcome_btn:
+                fragment = new WelcomeFragment();
+                moveToFragment(fragment);
+                break;
+            case R.id.time_line_btn:
+                break;
+            case R.id.venue_btn:
+                fragment = new MapScreen();
+                moveToFragment(fragment);
+                break;
+        }
+    }
+    private void moveToFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment, fragment.getClass().getSimpleName()).addToBackStack(null).commit();
 
     }
 }
