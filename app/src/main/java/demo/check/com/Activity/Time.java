2a95f1845a8b05fd2abfc2bd52f6822901496a4e
vikramvikraanth <vikram.m@abservetech.com>
2018-05-11 20:39:10 +0530

    basic completed

--- app/src/main/java/demo/check/com/Activity/Time.java
+++ app/src/main/java/demo/check/com/Activity/Time.java
@@ -1,9 +1,23 @@
 package demo.check.com.Activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.BaseAdapter;
 import android.widget.Toast;
+
+import com.bakerj.infinitecards.AnimationTransformer;
+import com.bakerj.infinitecards.CardItem;
+import com.bakerj.infinitecards.InfiniteCardView;
+import com.bakerj.infinitecards.ZIndexTransformer;
+import com.bakerj.infinitecards.transformer.DefaultTransformerToBack;
+import com.bakerj.infinitecards.transformer.DefaultTransformerToFront;
+import com.bakerj.infinitecards.transformer.DefaultZIndexTransformerCommon;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,36 +28,139 @@ import technolifestyle.com.imageslider.FlipperView;
 
 public class Time extends AppCompatActivity {
 
-    @BindView(R.id.flipper_layout)
-    FlipperLayout flipperLayout;
+    private InfiniteCardView mCardView;
+    private BaseAdapter mAdapter1, mAdapter2;
+    private int[] resId = {R.mipmap.pic1, R.mipmap.pic2, R.mipmap.pic3, R.mipmap
+            .pic4, R.mipmap.pic5};
+    private boolean mIsAdapter1 = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
         ButterKnife.bind(this);
-        setLayout();
         FontChangeCrawler fontChangeCrawler = new FontChangeCrawler(this.getAssets(), getString(R.string.app_font));
         fontChangeCrawler.replaceFonts((ViewGroup) this.findViewById(android.R.id.content));
+
+        mCardView = findViewById(R.id.view);
+        mAdapter1 = new MyAdapter(resId);
+        mAdapter2 = new MyAdapter(resId);
+        mCardView.setAdapter(mAdapter1);
+        mCardView.setCardAnimationListener(new InfiniteCardView.CardAnimationListener() {
+            @Override
+            public void onAnimationStart() {
+              //  Toast.makeText(Time.this, "Animation Start", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAnimationEnd() {
+                //Toast.makeText(Time.this, "Animation End", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        postdelay();
+    }
+    public void postdelay(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+                if (mIsAdapter1) {
+                    setStyle2();
+                    mCardView.bringCardToFront(mAdapter1.getCount() - 1);
+                } else {
+                    setStyle1();
+                    mCardView.bringCardToFront(mAdapter2.getCount() - 1);
+                }
+            }
+        }, 1500);
+    }
+    private void setStyle1() {
+        mCardView.setClickable(true);
+        mCardView.setAnimType(InfiniteCardView.ANIM_TYPE_FRONT);
+        mCardView.setAnimInterpolator(new LinearInterpolator());
+        mCardView.setTransformerToFront(new DefaultTransformerToFront());
+        mCardView.setTransformerToBack(new DefaultTransformerToBack());
+        mCardView.setZIndexTransformerToBack(new DefaultZIndexTransformerCommon());
     }
 
-    private void setLayout() {
-        String url[] = new String[]{
-                "http://blog.eap.ucop.edu/wp-content/uploads/2016/01/Julie-Huang-27.jpg",
-                "https://i.pinimg.com/originals/d3/84/d1/d384d1c565dc6b501a61286bf0879481.jpg",
-                "https://i.pinimg.com/originals/18/40/72/184072abb72399c23ab635faaa0a94ad.jpg"
-        };
-        for (int i = 0; i < 3; i++) {
-            FlipperView view = new FlipperView(getBaseContext());
-            view.setImageUrl(url[i])
-                    .setDescription("Cool" + (i + 1));
-            flipperLayout.addFlipperView(view);
-            view.setOnFlipperClickListener(new FlipperView.OnFlipperClickListener() {
-                @Override
-                public void onFlipperClick(FlipperView flipperView) {
-
+    private void setStyle2() {
+        mCardView.setClickable(true);
+        mCardView.setAnimType(InfiniteCardView.ANIM_TYPE_SWITCH);
+        mCardView.setAnimInterpolator(new OvershootInterpolator(-18));
+        mCardView.setTransformerToFront(new DefaultTransformerToFront());
+        mCardView.setTransformerToBack(new AnimationTransformer() {
+            @Override
+            public void transformAnimation(View view, float fraction, int cardWidth, int cardHeight, int fromPosition, int toPosition) {
+                int positionCount = fromPosition - toPosition;
+                float scale = (0.8f - 0.1f * fromPosition) + (0.1f * fraction * positionCount);
+                view.setScaleX(scale);
+                view.setScaleY(scale);
+                if (fraction < 0.5) {
+                    view.setRotationX(180 * fraction);
+                } else {
+                    view.setRotationX(180 * (1 - fraction));
                 }
-            });
+            }
+
+            @Override
+            public void transformInterpolatedAnimation(View view, float fraction, int cardWidth, int cardHeight, int fromPosition, int toPosition) {
+                int positionCount = fromPosition - toPosition;
+                float scale = (0.8f - 0.1f * fromPosition) + (0.1f * fraction * positionCount);
+                view.setTranslationY(-cardHeight * (0.8f - scale) * 0.5f - cardWidth * (0.02f *
+                        fromPosition - 0.02f * fraction * positionCount));
+            }
+        });
+        mCardView.setZIndexTransformerToBack(new ZIndexTransformer() {
+            @Override
+            public void transformAnimation(CardItem card, float fraction, int cardWidth, int cardHeight, int fromPosition, int toPosition) {
+                if (fraction < 0.4f) {
+                    card.zIndex = 1f + 0.01f * fromPosition;
+                } else {
+                    card.zIndex = 1f + 0.01f * toPosition;
+                }
+            }
+
+            @Override
+            public void transformInterpolatedAnimation(CardItem card, float fraction, int cardWidth, int cardHeight, int fromPosition, int toPosition) {
+
+            }
+        });
+    }
+
+    private static class MyAdapter extends BaseAdapter {
+        private int[] resIds = {};
+
+        MyAdapter(int[] resIds) {
+            this.resIds = resIds;
+        }
+
+        @Override
+        public int getCount() {
+            return resIds.length;
+        }
+
+        @Override
+        public Integer getItem(int position) {
+            return resIds[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout
+                        .item_card, parent, false);
+            }
+            convertView.setBackgroundResource(resIds[position]);
+            return convertView;
         }
     }
 }
